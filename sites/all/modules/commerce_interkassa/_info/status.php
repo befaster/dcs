dffe69751e1ba5ded82b517f2a49b50a3181f435
Dmitriy <swcphp@gmail.com>
2012-04-20 16:30:52 +0300

    del

---
+++ sites/all/modules/commerce_interkassa/_info/status.php
@@ -0,0 +1,4 @@
+<?php
+file_put_contents('status.txt', print_r($_POST, true));
+
+?>
---
+++ sites/all/modules/commerce_interkassa/_info/test.php
@@ -0,0 +1,8 @@
+<form name="payment" action="https://interkassa.com/lib/payment.php" method="post" 
+      enctype="application/x-www-form-urlencoded" accept-charset="cp1251">
+    <input type="hidden" name="ik_shop_id" value="C6C00382-2752-9476-9CED-3C766EA2BD10">
+    <input type="hidden" name="ik_payment_amount" value="0.10">
+    <input type="hidden" name="ik_payment_id" value="id1">
+    <input type="hidden" name="ik_payment_desc" value="description">
+    <input type="submit" name="process" value="Оплатить">
+</form>
